894fa5ece2f081ffb887024f49b648822befd8a0
seungheon <rumah800@naver.com>
2022-02-26 15:26:40 +0900

    delete preexisting code info

--- server/app.js
+++ server/app.js
@@ -52,54 +52,7 @@ let users = [
   },
 ];
 
-let codes = [
-  {
-    writer: '1234@naver.com',
-    contents: [
-      {
-        name: 'helloworld.cpp',
-        content: `#include <iostream>
-
-        int main() {
-          std::cout << "Hello, World!!" << std::endl;
-          return 0;
-        }`,
-      },
-      {
-        name: 'class.cpp',
-        content: `#include <iostream>
-        #include <string>
-        
-        class Base {
-          std::string s;
-        
-         public:
-          Base() : s("기반") { std::cout << "기반 클래스" << std::endl; }
-        
-          void what() { std::cout << s << std::endl; }
-        };
-        class Derived : public Base {
-          std::string s;
-        
-         public:
-          Derived() : s("파생"), Base() { std::cout << "파생 클래스" << std::endl; }
-        
-          void what() { std::cout << s << std::endl; }
-        };
-        int main() {
-          Base p;
-          Derived c;
-        
-          std::cout << "=== 포인터 버전 ===" << std::endl;
-          Base* p_c = &c;
-          p_c->what();
-        
-          return 0;
-        }`,
-      },
-    ],
-  },
-];
+let codes = [];
 
 app.post('/save', (req, res) => {
   const { fileName, code, user } = req.body;
